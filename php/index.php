68f60241c2bf14f9ae9af10b45d650fecb3ea5f4
Allison Amaral <allisonamaral94@gmail.com>
2016-01-06 11:29:56 -0800

    PHP Practice Update

--- php/index.php
+++ php/index.php
@@ -1,27 +1,10 @@
-<!--exclamation point + tab = get the base of HTML
-//emmet cheat sheet. Check it out. Not built in, have to get the sublime package unless on lab
-computers-->
-<?php
-//this is a universal variable to access the server
-$url = parse_url($_SERVER['REQUEST_URI']);
-//this will take whatever is in the URL and echo it. If I put localhost:9000/Gorgeous
-//it will say Hello Gorgeous!
-$name = substr($url['path'], 1);
-if(strlen($name) == 0) {
-    $name = 'World';
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Weather </title>
 </head>
 <body>
-    <!-- the question mark implies an echo statement-->
-    <!-- always html encode your work, to protect from scripting attacks -->
-    <h1>Hello <?= htmlentities($name) ?>!</h1>
+    
 </body>
 </html>
--- php/warmup.php
+++ php/warmup.php
@@ -1,12 +1,26 @@
 <?php
-$randInt = rand(1, 100);
-echo "Your new random value is $randInt\n";
-date_default_timezone_set("AMERICA/Los_Angeles");
 
-for ($x = 1; $x <= 12; $x++) {
-    $monthName = date('F', mktime(0, 0, 0, $x));
-    echo "$monthName ";
-    
+RandomInt();
+monthList();
+
+function RandomInt() {
+    $randInt = rand(1, 100);
+    echo "Your new random value is $randInt\n";
 }
 
+function monthList() {
+    date_default_timezone_set("AMERICA/Los_Angeles");
+    $monthArray = array();
+    echo "Date Sorted Month List\n";
+    for ($monthNum = 1; $monthNum <= 12; $monthNum++) {
+        $monthName = date('F', mktime(0, 0, 0, $monthNum));
+        $monthArray[$monthNum] = $monthName;
+        echo "$monthName \n";
+    }
+    sort($monthArray);
+    echo "Alphabetically Sorted Month List\n";
+    for($sortedMonth = 1; $sortedMonth <= 12; $sortedMonth++) {
+        echo "$monthArray[$sortedMonth] \n";
+    }
+}
 ?>
